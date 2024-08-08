2663b98fb32c1d5f8ace0fb6abc8d92a45bedc42
Harish k <144902152+KHarish5544@users.noreply.github.com>
2024-08-08 12:09:08 +0530

    Add files via upload

---
+++ lab-3/Server.js
@@ -0,0 +1,28 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const port = 4000;
+const server = http.createServer((req, res) => { // Serve the index.html file for GET requests to root
+if (req.method==='GET' && req.url==='/') { 
+    fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
+        if (err) { 
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
+            res.end('Server Error');
+            res.end(data); 
+            return;
+        }
+
+        res.writeHead(200, { 'Content-Type': 'text/html' }); 
+        res.end(data); 
+    });
+}
+else if (req.method == 'POST' && req.url == '/post') { 
+    res.writeHead(200, { 'Content-Type': 'text/plain' }); 
+    res.end('This is a POST request.'); }
+else {
+    res.writeHead(405, { 'Content-Type': 'text/plain' }); 
+    res.end('Method Not Allowed'); }
+}); 
+server.listen(port, () => { 
+    console.log(`Server is listening on http://127.1.0.1:${port}/`); 
+});
